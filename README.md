547c2338d4d6c50207b81ff7d9a4ba19d80810c9
Frederik Held <git@frederikheld.de>
2021-04-05 13:31:36 +0200

    Initial scaffolding

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # nm-dbus
+
 Provides simplified access to NetworkManager features via DBus
+
+> Note: This is in a very very early stage and not recommended for any other use than contributing to development!
---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const NetworkManager = require('./lib/network-manager')
+const Device = require('./lib/device')
+const Connection = require('./lib/connection')
+
+/**
+ * SYLE A
+ * 
+ * Usage:
+ * 
+ * const NetworkManager = require('NetworkManager')
+ * 
+ * const networkManager = new NetworkManager()
+ * const device = new NetworkManager.Device()
+ */
+module.exports = NetworkManager
+
+
+/**
+ * SYLE B
+ * 
+ * Usage:
+ * 
+ * const { NetworkManager, Device } = require('NetworkManager')
+ * 
+ * const networkManager = new NetworkManager()
+ * const device = new Device()
+ */
+module.exports.NetworkManager = NetworkManager
+module.exports.Device = Device
+module.exports.Connection = Connection
