12fdfd19f8cc9a88a05389f45729f5146dc9dc76
Frederik Held <git@frederikheld.de>
2021-04-07 00:16:56 +0200

    Should implement everything that was there before

--- experiments/nm.device.access-points.js
+++ experiments/nm.device.access-points.js
@@ -10,23 +10,30 @@ async function main () {
 
     console.log('Fetching devices ...')
     const devices = await nm.devices.getAll()
+    // const devices = await nm.getDevices()
     // console.log('DEVICES:', devices)
     console.log('Done.')
+
+    const wirelessDevices = await nm.devices.getWireless()
+    console.log('# of wireless devices', wirelessDevices.length)
     
     // Check for each device if it has wireless capabilities:
-    devices.forEach(async (device, i) => {
+    for (const [i, device] of devices.entries()) {
         console.log(i, await device.hasWirelessInterface())
-    })
-    return
+    }
 
-    // pick a wireless device from list as an example:
+    // Check if wireless is enabled:
+    console.log('wireless enabled:', await nm.wireless.isEnabled())
 
-    const device = devices[3]
+    // pick first wireless device from list as an example:
+
+    const device = wirelessDevices[0]
+    console.log(await device.hasWirelessInterface())
 
     // console.log('dbusConfig:', device.accessPoints.dbusConfig)
 
     console.log('Fetching available access points ...')
-    const accessPoints = await device.accessPoints.list()
+    const accessPoints = await device.accessPoints.listAll()
     console.log('ACCESS POINTS:', accessPoints)
 
     console.log(Date.now())
