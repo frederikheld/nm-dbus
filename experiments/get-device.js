a9126b576448bc73901190c6d34132e1080e996d
Frederik Held <git@frederikheld.de>
2021-04-20 01:06:21 +0200

    Adds Devices.getByInterface() method

---
+++ experiments/get-device.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const NetworkManager = require('../lib/network-manager')
+
+async function main () {
+
+    const nm = new NetworkManager()
+
+    // get all devices and list only iface name:
+    const devices = await nm.devices.getAll()
+    for (const device of devices) {
+        console.log(device.interface)
+    }
+
+    console.log('---')
+
+    // get specific device by interface name:
+    const ifaceName = devices[devices.length - 1].interface // take the last one in the list
+    const myDevice = await nm.devices.getByInterface(ifaceName)
+    console.log(myDevice.interface)
+
+}
+
+main()
