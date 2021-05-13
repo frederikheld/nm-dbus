325a874c73b1ec42757c9dadd1cdddfdb0c2cfa2
Frederik Held <git@frederikheld.de>
2021-05-14 01:10:56 +0200

    Implements Device.disconnect() which truly deactivates all connections on the device

--- experiments/create-ap/start-stop-wireless-ap.js
+++ experiments/create-ap/start-stop-wireless-ap.js
@@ -9,19 +9,16 @@ async function main () {
     const deviceInterface = 'wlx1cbfce4d80e9'
     const selectedDevice = await nm.devices.getByInterface(deviceInterface)
 
-    const additionalDbusSettings = {
-        connection: {
-            autoconnect: false
-        }
-    }
-    
-    const connection = await nm.connections.createWifiAccessPoint('test-ap', 'secret-password', additionalDbusSettings)
+    const connection = await nm.connections.createWifiAccessPoint('test-ap', 'secret-password')
 
     const activeConnectionPath = await nm.activateConnection(connection, selectedDevice)
 
     console.log(activeConnectionPath)
 
-    nm.disconnect()
+    setTimeout(async () => {
+        await selectedDevice.disconnect()
+        await nm.disconnect()
+    }, 1000)
 }
 
 main()
